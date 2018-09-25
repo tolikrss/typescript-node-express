149b7525b819c0ea36db6a937fd57596908bc867
tolikrss <tolikrss@gmail.com>
2018-09-25 17:56:53 +0300

    add redux and simple redux for front

---
+++ front/src/containers/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './home.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        Home
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ front/src/containers/no-routes/NoRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './no-routes.css';
+
+class NoRoutes extends React.Component {
+  render() {
+    return (
+      <div className="NoRoutes">
+	      NoRoutes
+      </div>
+    );
+  }
+}
+
+export default NoRoutes;

279051a673deaec0077fbe24c9e3cd9dcaa8eb9e
tolikrss <tolikrss@gmail.com>
2018-10-02 13:05:07 +0300

    add router v.4 (small update)

--- front/src/components/app/App.js
+++ front/src/components/app/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 import {connect} from 'react-redux';
-import {simpleAction} from "../../actions/simpleAction";
+import {simpleAction} from '../../actions/simpleAction';
+import {Link, withRouter} from 'react-router-dom';
+import {compose} from 'redux';
 
 class App extends React.Component {
 	simpleAction = (event) => {
@@ -10,18 +12,27 @@ class App extends React.Component {
 	};
 
 	render() {
+		const {match, location, history} = this.props;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo"/>
 					<h1 className="App-title">Welcome to React</h1>
 				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
 
 				<button onClick={this.simpleAction}>Test redux action</button>
 				<pre> {JSON.stringify(this.props)}</pre>
+				<br/>
+
+				<div>You are now at {location.pathname}</div>
+				<div onClick={() => this.props.history.push('/404')}>test router history</div>
+
+				<Link to="/">
+					<button>home</button>
+				</Link>
+				<Link to="/info">
+					<button>info</button>
+				</Link>
 			</div>
 		);
 	}
@@ -35,4 +46,7 @@ const mapDispatchToProps = dispatch => ({
 	simpleAction: () => dispatch(simpleAction())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default compose(
+	connect(mapStateToProps, mapDispatchToProps),
+	withRouter
+)(App);
--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
-import configureStore from "./store/store";
-import {Provider} from "react-redux";
+import configureStore from './store/store';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Home from './containers/home/Home';
+import Info from './containers/info/Info';
 
 ReactDOM.render(
-
 	<Provider store={configureStore()}>
-		<App />
+		<Router>
+			<div>
+				<App/>
+				<div>
+					<Route exact path="/" component={Home}/>
+					<Route exact path="/info" component={Info}/>
+				</div>
+			</div>
+		</Router>
 	</Provider>,
-	document.getElementById('root'));
+	document.getElementById('root')
+);
+
 registerServiceWorker();

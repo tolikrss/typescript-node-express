48e8dc533fcdb3c20d5f6d1848fcdf7b7de322a5
Anatoliy Lakhno <tolikrss@gmail.com>
2018-11-02 16:51:29 +0200

    small update controller

--- server/routes/controllers/films.controller.ts
+++ server/routes/controllers/films.controller.ts
@@ -5,46 +5,52 @@ import { FilmsService } from '../../services/films.service';
 
 export class FilmsController {
 
-    public static async listFilms( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let films = await FilmsService.listFilms();
-        res.json( films );
-        return films;
-    }
+  public static async listFilms( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const films = await FilmsService.listFilms();
 
-    public static async listFilmsFindTitle( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let findedFilms = await FilmsService.listFilmsFindTitle( req.params.id );
-        res.json( findedFilms );
-        return findedFilms;
-    }
+    res.json( films );
+    return films;
+  }
 
-    public static async listFilmsFindStars( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let findedFilms = await FilmsService.listFilmsFindStars( req.params.id );
-        res.json( findedFilms );
-        return findedFilms;
-    }
+  public static async listFilmsFindTitle( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const findedFilms = await FilmsService.listFilmsFindTitle( req.params.id );
 
-    public static async createFilm( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let createdFilm = await FilmsService.createFilm( req.body );
-        res.json( createdFilm );
-        return createdFilm;
-    }
+    res.json( findedFilms );
+    return findedFilms;
+  }
 
-    public static async deleteFilm( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let deletedFilm = await FilmsService.deleteFilm( req.params.id );
-        res.json( deletedFilm );
-        return deletedFilm;
-    }
+  public static async listFilmsFindStars( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const findedFilms = await FilmsService.listFilmsFindStars( req.params.id );
 
-    public static async deleteAllFilms( req: Request, res: Response ): Promise<any> {
-        const queryParams = req.query;
-        let deletedFilms = await FilmsService.deleteAllFilms();
-        res.json( deletedFilms );
-        return deletedFilms;
-    }
+    res.json( findedFilms );
+    return findedFilms;
+  }
+
+  public static async createFilm( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const createdFilm = await FilmsService.createFilm( req.body );
+
+    res.json( createdFilm );
+    return createdFilm;
+  }
+
+  public static async deleteFilm( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const deletedFilm = await FilmsService.deleteFilm( req.params.id );
+
+    res.json( deletedFilm );
+    return deletedFilm;
+  }
+
+  public static async deleteAllFilms( req: Request, res: Response ): Promise<any> {
+    const queryParams = req.query;
+    const deletedFilms = await FilmsService.deleteAllFilms();
+
+    res.json( deletedFilms );
+    return deletedFilms;
+  }
 
 }
